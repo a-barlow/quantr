7ff30817dd786cc681577c5df05bd87b2a8ee1b2
Andrew Barlow <a.barlow.dev@gmail.com>
2023-10-24 14:08:48 +0100

    Add grovers test
    Adds an external test. This test is an algorithm for confirming a row of
    3x3 sudoku. For the testing practicality, it is a 10 qubit circuit with
    two custom functions and several hadamard gates. This should also
    compile and run quickly enough, in addition to having enough complexity,
    to be regarded as a good practical test of quantr.
    
    Signed-off-by: Andrew Barlow <a.barlow.dev@gmail.com>

--- tests/grovers.rs
+++ tests/grovers.rs
@@ -9,7 +9,7 @@
 */
 
 use quantr::circuit::{
-    states::SuperPosition,
+    states::{SuperPosition, Qubit, ProductState},
     Circuit,
     Measurement::{NonObservable, Observable},
     StandardGate,
@@ -75,6 +75,141 @@ fn grovers_3qubit() {
                 _ => assert_eq!(count, 0usize),
             }
         }
+    }
+}
+
+
+const CCC_NUMBER: usize = 4;
+const CCCCC_NUMBER: usize = 6;
+
+#[test]
+fn x3sudoko() {
+    let mut qc: Circuit = Circuit::new(10).unwrap();
+
+    qc.add_repeating_gate(StandardGate::H, vec![0, 1, 2, 3, 4, 5])
+        .unwrap();
+    qc.add_gate(StandardGate::X, 8).unwrap();
+    qc.add_gate(StandardGate::X, 9).unwrap();
+    qc.add_gate(StandardGate::H, 9).unwrap();
+
+    // oracle building
+    for i in 0..=2 {
+        qc.add_gate(StandardGate::Toffoli(i, i+3), 8).unwrap();
+    }
+    qc.add_gate(
+        StandardGate::Custom(cccnot, &[0, 1, 2], "X".to_string()),
+        6,
+    )
+    .unwrap();
+    for i in 0..=2 {
+        qc.add_gate(StandardGate::CNot(i), 6).unwrap();
+    }
+    qc.add_gate(
+        StandardGate::Custom(cccnot, &[3, 4, 5], "X".to_string()),
+        7,
+    )
+    .unwrap();
+    for i in 3..=5 {
+        qc.add_gate(StandardGate::CNot(i), 7).unwrap();
+    }
+
+    // The phase kickback
+    qc.add_gate(
+        StandardGate::Custom(cccnot, &[6, 7, 8], "X".to_string()),
+        9,
+    )
+    .unwrap();
+
+
+    // Reset by using the oracle again
+    for i in 0..=2 {
+        qc.add_gate(StandardGate::Toffoli(i, i+3), 8).unwrap();
+    }
+    qc.add_gate(
+        StandardGate::Custom(cccnot, &[0, 1, 2], "X".to_string()),
+        6,
+    )
+    .unwrap();
+    for i in 0..=2 {
+        qc.add_gate(StandardGate::CNot(i), 6).unwrap();
+    }
+    qc.add_gate(
+        StandardGate::Custom(cccnot, &[3, 4, 5], "X".to_string()),
+        7,
+    )
+    .unwrap();
+    for i in 3..=5 {
+        qc.add_gate(StandardGate::CNot(i), 7).unwrap();
+    }
+
+
+    // Amplitude amplification
+    qc.add_repeating_gate(StandardGate::H, vec![0, 1, 2, 3, 4, 5])
+        .unwrap();
+    qc.add_repeating_gate(StandardGate::X, vec![0, 1, 2, 3, 4, 5])
+        .unwrap();
+    qc.add_gate(StandardGate::H, 5).unwrap();
+    qc.add_gate(
+        StandardGate::Custom(cccccnot, &[0, 1, 2, 3, 4], "X".to_string()),
+        5,
+    ).unwrap();
+    qc.add_gate(StandardGate::H, 5).unwrap();
+    qc.add_repeating_gate(StandardGate::X, vec![0, 1, 2, 3,4,5])
+        .unwrap();
+    qc.add_repeating_gate(StandardGate::H, vec![0, 1, 2,3,4,5])
+        .unwrap();
+    // END
+
+    qc.simulate();
+
+    if let Observable(bin_count) = qc.repeat_measurement(5000).unwrap() {
+        for (state, count) in bin_count {
+            match &state.as_string()[0..=5] {
+                "001100"
+                | "001010"
+                | "010100" 
+                | "010001" 
+                | "100010"
+                | "100001" 
+                => assert!(count > 150usize),
+                _ => assert!(count < 150usize),
+            }
+        }
+    }
+}
+
+fn cccnot(input_state: ProductState) -> SuperPosition {
+    let mut copy_state = input_state.clone();
+    if copy_state.state == [Qubit::One; CCC_NUMBER] {
+        copy_state.state[CCC_NUMBER-1] = Qubit::Zero;
+        return copy_state.to_super_position();
+    } else if copy_state.state == {
+        let mut temp = [Qubit::One; CCC_NUMBER];
+        temp[CCC_NUMBER-1] = Qubit::Zero;
+        temp
+    } {
+        copy_state.state[CCC_NUMBER-1] = Qubit::One;
+        return copy_state.to_super_position();
+    } else {
+        copy_state.to_super_position()
+    }
+}
+
+// Implementation of a 5 controlled toffoli gate
+fn cccccnot(input_state: ProductState) -> SuperPosition {
+    let mut copy_state = input_state.clone();
+    if copy_state.state == [Qubit::One; CCCCC_NUMBER] {
+        copy_state.state[CCCCC_NUMBER-1] = Qubit::Zero;
+        return copy_state.to_super_position();
+    } else if copy_state.state == {
+        let mut temp = [Qubit::One; CCCCC_NUMBER];
+        temp[CCCCC_NUMBER-1] = Qubit::Zero;
+        temp
+    } {
+        copy_state.state[CCCCC_NUMBER-1] = Qubit::One;
+        return copy_state.to_super_position();
+    } else {
+        copy_state.to_super_position()
     }
 }
 
