f808d2754e23e38a4fca9d09886791698bfbba5e
Andrew Barlow <a.barlow.dev@gmail.com>
2023-10-27 13:02:26 +0100

    Update docs
    Update the quick start guide to note that the code is found in the
    examples folder, and it can be ran with cargo.
    
    Signed-off-by: Andrew Barlow <a.barlow.dev@gmail.com>

--- examples/grovers.rs
+++ examples/grovers.rs
@@ -1,3 +1,13 @@
+/*
+* Copyright (c) 2023 Andrew Rowan Barlow. Licensed under the EUPL-1.2
+* or later. You may obtain a copy of the licence at
+* https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12. A copy 
+* of the EUPL-1.2 licence in English is given in LICENCE.txt which is 
+* found in the root directory of this repository.
+*
+* Author: Andrew Rowan Barlow <a.barlow.dev@gmail.com> 
+*/
+
 use quantr::circuit::{printer::Printer, Circuit, Measurement, StandardGate};
 
 fn main() {
